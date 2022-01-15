d66685638df19aeeaf7e6336886bc85cf0bb2017
bnidia mac <bnidia@student.21-school.ru>
2022-01-15 20:22:01 +0300

    git add -f test.sh! sh

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+clear
+echo "******************"
+echo "*                *"
+echo "*  war-machine   *"
+echo "*                *"
+echo "******************"
+#cd /Users/r/gnl-war-machine-v2019
+#./grademe.sh
+echo ""
+echo ""
+echo "******************"
+echo "*                *"
+echo "* gnlTester      *"
+echo "*    Tripouille  *"
+echo "******************"
+cd /Users/r/gnlTester-Tripouille
+make m
