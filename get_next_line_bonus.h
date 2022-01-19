55382b25f4a9dec3b4020f863abb2c18eae015b8
bnidia <bnidia@bnidia-ubuntu.(none)>
2022-01-20 00:37:16 +0300

    Ready to test on mac2

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -11,7 +11,7 @@
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_BONUS_H
-# define GET_NEXT_LINE__BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1024
 # endif
